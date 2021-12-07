dab5e1658bb45eda8c2b7730fabeac79eac90cea
Jiahe <yi.jiahe00@gmail.com>
2021-12-07 20:28:00 +0800

    Update day 2 tests

--- test/day_2_test.exs
+++ test/day_2_test.exs
@@ -1,22 +1,28 @@
 defmodule DayTwoTest do
   use ExUnit.Case
 
-  test "compute example programs" do
+  test "Part One example 1" do
     intcode = [1,9,10,3,2,3,11,0,99,30,40,50]
     {answer, intcode_result_part_one} = DayTwo.parse_intcode(0, intcode)
     assert answer == 3500
     assert intcode_result_part_one == [3500,9,10,70,2,3,11,0,99,30,40,50]
+  end
 
+  test "Part One example 2" do
     intcode = [1,0,0,0,99]
     {answer, intcode_result_part_one} = DayTwo.parse_intcode(0, intcode)
     assert answer == 2
     assert intcode_result_part_one == [2,0,0,0,99]
+  end
 
+  test "Part One example 3" do
     intcode = [2,4,4,5,99,0]
     {answer, intcode_result_part_one} = DayTwo.parse_intcode(0, intcode)
     assert answer == 2
     assert intcode_result_part_one == [2,4,4,5,99,9801]
+  end
 
+  test "Part One example 4" do
     intcode = [1,1,1,4,99,5,6,0,99]
     {answer, intcode_result_part_one} = DayTwo.parse_intcode(0, intcode)
     assert answer == 30
