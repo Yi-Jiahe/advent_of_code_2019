18f06483191e8fba2054221ee089613fd8e4faa2
Jiahe <yi.jiahe00@gmail.com>
2021-12-07 11:57:02 +0800

    Formatting

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -26,24 +26,23 @@ defmodule DayOne do
   end
 
   def main(filename \\ "./puzzle_inputs/day_1/module_masses.txt") do
-  case File.read(filename) do
-    {:ok, body} ->
-      module_masses = body
-      |> String.split
-      |> Enum.map(&Integer.parse/1)
-      |> Enum.map(fn {mass, _} -> mass end)
+    case File.read(filename) do
+      {:ok, body} ->
+        module_masses = body
+        |> String.split
+        |> Enum.map(&Integer.parse/1)
+        |> Enum.map(fn {mass, _} -> mass end)
 
-      part_one_sum = module_masses
-      |> Enum.map(&PartOne.calculate_fuel_for_module/1)
-      |> Enum.sum
-      IO.puts("The sum of the fuel requirements for all the modules on the spacecraft is #{part_one_sum}.")
+        part_one_sum = module_masses
+        |> Enum.map(&PartOne.calculate_fuel_for_module/1)
+        |> Enum.sum
+        IO.puts("The sum of the fuel requirements for all the modules on the spacecraft is #{part_one_sum}.")
 
-      part_two_sum = module_masses
-      |> Enum.map((&PartTwo.calculate_fuel_for_module/1))
-      |> Enum.sum
-      IO.puts("The sum of the fuel requirements for all the modules on the spacecraft, taking into account the mass of the added fuel is #{part_two_sum}.")
-    {:error, reason} -> IO.puts("Error: #{reason}")
+        part_two_sum = module_masses
+        |> Enum.map((&PartTwo.calculate_fuel_for_module/1))
+        |> Enum.sum
+        IO.puts("The sum of the fuel requirements for all the modules on the spacecraft, taking into account the mass of the added fuel is #{part_two_sum}.")
+      {:error, reason} -> IO.puts("Error: #{reason}")
     end
   end
-
 end
