9663123322b396bb7069bc46e630724bbaa2ab73
Jiahe <yi.jiahe00@gmail.com>
2021-12-07 14:35:45 +0800

    Finish Day 3

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -1,180 +1,378 @@
 defmodule DayThree do
-  def find_manhattan_distance_of_closest_intersection(intersections) do
-    {point, intersections} = List.pop_at(intersections, 0)
-    distance = manhattan_distance_from_origin(point)
-    if Enum.empty?(intersections) do
-      distance
-    else
-      find_manhattan_distance_of_closest_intersection(intersections, distance)
+  defmodule PartOne do
+    def find_manhattan_distance_of_closest_intersection(intersections) do
+      {point, intersections} = List.pop_at(intersections, 0)
+      distance = manhattan_distance_from_origin(point)
+
+      if Enum.empty?(intersections) do
+        distance
+      else
+        find_manhattan_distance_of_closest_intersection(intersections, distance)
+      end
     end
-  end
 
-  def find_manhattan_distance_of_closest_intersection(intersections, shortest_distance) do
-    {point, intersections} = List.pop_at(intersections, 0)
-    distance = manhattan_distance_from_origin(point)
-    shortest_distance = min(distance, shortest_distance)
-    if Enum.empty?(intersections) do
-      shortest_distance
-    else
-      find_manhattan_distance_of_closest_intersection(intersections, shortest_distance)
+    def find_manhattan_distance_of_closest_intersection(intersections, shortest_distance) do
+      {point, intersections} = List.pop_at(intersections, 0)
+      distance = manhattan_distance_from_origin(point)
+      shortest_distance = min(distance, shortest_distance)
+
+      if Enum.empty?(intersections) do
+        shortest_distance
+      else
+        find_manhattan_distance_of_closest_intersection(intersections, shortest_distance)
+      end
     end
-  end
 
-  def manhattan_distance_from_origin(point) do
-    {x, y} = {Enum.at(point, 0), Enum.at(point, 1)}
-    abs(x) + abs(y)
-  end
-
-  def update_intersections(point, path_1_points, intersections) do
-    if MapSet.member?(path_1_points, point) do
-      MapSet.put(intersections, point)
-    else
-      intersections
+    def manhattan_distance_from_origin(point) do
+      {x, y} = {Enum.at(point, 0), Enum.at(point, 1)}
+      abs(x) + abs(y)
     end
-  end
 
-  @doc """
-  Moves the first wire along the segment, adding to the wire points as it goes.
-  """
-  def move(point, points, index, direction, start_value, end_value) do
-    new_value = start_value + direction
-    point = List.replace_at(point, index, new_value)
-    if new_value == end_value do
-      {point, points}
-    else
-      move(point, MapSet.put(points, point), index, direction, new_value, end_value)
+    def update_intersections(point, path_1_points, intersections) do
+      if MapSet.member?(path_1_points, point) do
+        MapSet.put(intersections, point)
+      else
+        intersections
+      end
     end
-  end
-
-  @doc """
-  Moves the second wire along each segment, returning any intersections with the first wire's path.
-  """
-  def move(point, path_1_points, index, direction, start_value, end_value, intersections) do
-    new_value = start_value + direction
-    point = List.replace_at(point, index, new_value)
-    intersections = update_intersections(point, path_1_points, intersections)
-    if new_value == end_value do
-      {point, intersections}
-    else
-      move(point, path_1_points, index, direction, new_value, end_value, intersections)
-    end
-  end
-
-  @doc """
-  Moves the first wire along its path, returning the points on each segment.
-  """
-  def move_segment(point, segment) do
-    {direction, x} = String.split_at(segment, 1)
-    {x, _} = Integer.parse(x)
-
-    case direction do
-      "U" ->
-        # IO.puts("Up #{x}")
-        current_y = Enum.at(point, 1)
-        move(point, MapSet.new(), 1, 1, current_y, current_y + x)
-      "D" ->
-        # IO.puts("Down #{x}")
-        current_y = Enum.at(point, 1)
-        move(point, MapSet.new(), 1, -1, current_y, current_y - x)
-      "L" ->
-        # IO.puts("Left #{x}")
-        current_x = Enum.at(point, 0)
-        move(point, MapSet.new(), 0, -1, current_x, current_x - x)
-      "R" ->
-        # IO.puts("Right #{x}")
-        current_x = Enum.at(point, 0)
-        move(point, MapSet.new(), 0, 1, current_x, current_x + x)
-    end
-  end
-
-  @doc """
-  Moves the second wire along its path, returning any intersections with the first wire's path along the segment.
-  """
-  def move_segment(point, path_1_points, segment) do
-    {direction, x} = String.split_at(segment, 1)
-    {x, _} = Integer.parse(x)
-
-    case direction do
-      "U" ->
-        # IO.puts("Up #{x}")
-        current_y = Enum.at(point, 1)
-        move(point, path_1_points, 1, 1, current_y, current_y + x, MapSet.new())
-      "D" ->
-        # IO.puts("Down #{x}")
-        current_y = Enum.at(point, 1)
-        move(point, path_1_points, 1, -1, current_y, current_y - x, MapSet.new())
-      "L" ->
-        # IO.puts("Left #{x}")
-        current_x = Enum.at(point, 0)
-        move(point, path_1_points, 0, -1, current_x, current_x - x, MapSet.new())
-      "R" ->
-        # IO.puts("Right #{x}")
-        current_x = Enum.at(point, 0)
-        move(point, path_1_points, 0, 1, current_x, current_x + x, MapSet.new())
-    end
-  end
-
-  @doc """
-  Moves the first wire along its path, returning the points along the path.
-  """
-  def move_path(path, point, points) do
-    {segment, path} = List.pop_at(path, 0)
-    {point, points_new} = move_segment(point, segment)
-    points = MapSet.union(points, points_new)
-    if Enum.empty?(path) do
-      points
-    else
-      move_path(path, point, points)
-    end
-  end
 
     @doc """
-  Moves the second wire along its path, returning all intersections with the first
-  """
-  def move_path(path, point, path_1_points, intersections) do
-    {segment, path} = List.pop_at(path, 0)
-    {point, intersections_new} = move_segment(point, path_1_points, segment)
-    intersections = MapSet.union(intersections, intersections_new)
-    if Enum.empty?(path) do
-      intersections
-    else
-      move_path(path, point, path_1_points, intersections)
+    Moves the first wire along the segment, adding to the wire points as it goes.
+    """
+    def move(point, points, index, direction, start_value, end_value) do
+      new_value = start_value + direction
+      point = List.replace_at(point, index, new_value)
+
+      if new_value == end_value do
+        {point, points}
+      else
+        move(point, MapSet.put(points, point), index, direction, new_value, end_value)
+      end
+    end
+
+    @doc """
+    Moves the second wire along each segment, returning any intersections with the first wire's path.
+    """
+    def move(point, path_1_points, index, direction, start_value, end_value, intersections) do
+      new_value = start_value + direction
+      point = List.replace_at(point, index, new_value)
+      intersections = update_intersections(point, path_1_points, intersections)
+
+      if new_value == end_value do
+        {point, intersections}
+      else
+        move(point, path_1_points, index, direction, new_value, end_value, intersections)
+      end
+    end
+
+    @doc """
+    Moves the first wire along its path, returning the points on each segment.
+    """
+    def move_segment(point, segment) do
+      {direction, x} = String.split_at(segment, 1)
+      {x, _} = Integer.parse(x)
+
+      case direction do
+        "U" ->
+          # IO.puts("Up #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, MapSet.new(), 1, 1, current_y, current_y + x)
+
+        "D" ->
+          # IO.puts("Down #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, MapSet.new(), 1, -1, current_y, current_y - x)
+
+        "L" ->
+          # IO.puts("Left #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, MapSet.new(), 0, -1, current_x, current_x - x)
+
+        "R" ->
+          # IO.puts("Right #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, MapSet.new(), 0, 1, current_x, current_x + x)
+      end
+    end
+
+    @doc """
+    Moves the second wire along its path, returning any intersections with the first wire's path along the segment.
+    """
+    def move_segment(point, path_1_points, segment) do
+      {direction, x} = String.split_at(segment, 1)
+      {x, _} = Integer.parse(x)
+
+      case direction do
+        "U" ->
+          # IO.puts("Up #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, path_1_points, 1, 1, current_y, current_y + x, MapSet.new())
+
+        "D" ->
+          # IO.puts("Down #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, path_1_points, 1, -1, current_y, current_y - x, MapSet.new())
+
+        "L" ->
+          # IO.puts("Left #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, path_1_points, 0, -1, current_x, current_x - x, MapSet.new())
+
+        "R" ->
+          # IO.puts("Right #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, path_1_points, 0, 1, current_x, current_x + x, MapSet.new())
+      end
+    end
+
+    @doc """
+    Moves the first wire along its path, returning the points along the path.
+    """
+    def move_path(path, point, points) do
+      {segment, path} = List.pop_at(path, 0)
+      {point, points_new} = move_segment(point, segment)
+      points = MapSet.union(points, points_new)
+
+      if Enum.empty?(path) do
+        points
+      else
+        move_path(path, point, points)
+      end
+    end
+
+    @doc """
+    Moves the second wire along its path, returning all intersections with the first
+    """
+    def move_path(path, point, path_1_points, intersections) do
+      {segment, path} = List.pop_at(path, 0)
+      {point, intersections_new} = move_segment(point, path_1_points, segment)
+      intersections = MapSet.union(intersections, intersections_new)
+
+      if Enum.empty?(path) do
+        intersections
+      else
+        move_path(path, point, path_1_points, intersections)
+      end
+    end
+
+    @doc """
+    Find a MapSet of points along the first wire's path
+    """
+    def find_wire_points(path) do
+      move_path(path, [0, 0], MapSet.new())
+    end
+
+    def find_intersections(paths) do
+      [path_1, path_2] = paths
+      path_1_points = find_wire_points(path_1)
+      intersections = move_path(path_2, [0, 0], path_1_points, MapSet.new())
+      MapSet.to_list(intersections)
     end
   end
 
-  @doc """
-  Find a MapSet of points along the first wire's path
-  """
-  def find_wire_points(path) do
-    move_path(path, [0, 0], MapSet.new())
+  defmodule PartTwo do
+    def update_intersections(point, path_1_points, intersections, steps) do
+      if Map.has_key?(path_1_points, point) do
+        total_steps = steps + Map.get(path_1_points, point)
+
+        if Map.has_key?(intersections, point) do
+          if total_steps >= Map.get(intersections, point) do
+            intersections
+          else
+            Map.replace(intersections, point, total_steps)
+          end
+        else
+          Map.put(intersections, point, total_steps)
+        end
+      else
+        intersections
+      end
+    end
+
+    @doc """
+    Moves the first wire along the segment, adding to the wire points as it goes.
+    """
+    def move(point, points, index, direction, start_value, end_value, steps) do
+      new_value = start_value + direction
+      point = List.replace_at(point, index, new_value)
+      new_steps = steps + 1
+
+      if new_value == end_value do
+        {point, points, new_steps}
+      else
+        move(
+          point,
+          Map.put_new(points, point, new_steps),
+          index,
+          direction,
+          new_value,
+          end_value,
+          new_steps
+        )
+      end
+    end
+
+    @doc """
+    Moves the second wire along each segment, returning any intersections with the first wire's path.
+    """
+    def move(point, path_1_points, index, direction, start_value, end_value, intersections, steps) do
+      new_value = start_value + direction
+      point = List.replace_at(point, index, new_value)
+      new_steps = steps + 1
+      intersections = update_intersections(point, path_1_points, intersections, new_steps)
+
+      if new_value == end_value do
+        {point, intersections, new_steps}
+      else
+        move(
+          point,
+          path_1_points,
+          index,
+          direction,
+          new_value,
+          end_value,
+          intersections,
+          new_steps
+        )
+      end
+    end
+
+    @doc """
+    Moves the first wire along its path, returning the points on each segment.
+    """
+    def move_segment(point, segment, steps) do
+      {direction, x} = String.split_at(segment, 1)
+      {x, _} = Integer.parse(x)
+
+      case direction do
+        "U" ->
+          # IO.puts("Up #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, Map.new(), 1, 1, current_y, current_y + x, steps)
+
+        "D" ->
+          # IO.puts("Down #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, Map.new(), 1, -1, current_y, current_y - x, steps)
+
+        "L" ->
+          # IO.puts("Left #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, Map.new(), 0, -1, current_x, current_x - x, steps)
+
+        "R" ->
+          # IO.puts("Right #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, Map.new(), 0, 1, current_x, current_x + x, steps)
+      end
+    end
+
+    @doc """
+    Moves the second wire along its path, returning any intersections with the first wire's path along the segment.
+    """
+    def move_segment(point, path_1_points, segment, steps) do
+      {direction, x} = String.split_at(segment, 1)
+      {x, _} = Integer.parse(x)
+
+      case direction do
+        "U" ->
+          # IO.puts("Up #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, path_1_points, 1, 1, current_y, current_y + x, Map.new(), steps)
+
+        "D" ->
+          # IO.puts("Down #{x}")
+          current_y = Enum.at(point, 1)
+          move(point, path_1_points, 1, -1, current_y, current_y - x, Map.new(), steps)
+
+        "L" ->
+          # IO.puts("Left #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, path_1_points, 0, -1, current_x, current_x - x, Map.new(), steps)
+
+        "R" ->
+          # IO.puts("Right #{x}")
+          current_x = Enum.at(point, 0)
+          move(point, path_1_points, 0, 1, current_x, current_x + x, Map.new(), steps)
+      end
+    end
+
+    @doc """
+    Moves the first wire along its path, returning the points along the path.
+    """
+    def move_path(path, point, points, steps) do
+      {segment, path} = List.pop_at(path, 0)
+      {point, points_new, steps_new} = move_segment(point, segment, steps)
+      points = Map.merge(points_new, points)
+
+      if Enum.empty?(path) do
+        points
+      else
+        move_path(path, point, points, steps_new)
+      end
+    end
+
+    @doc """
+    Moves the second wire along its path, returning all intersections with the first
+    """
+    def move_path(path, point, path_1_points, intersections, steps) do
+      {segment, path} = List.pop_at(path, 0)
+      {point, intersections_new, new_steps} = move_segment(point, path_1_points, segment, steps)
+      intersections = Map.merge(intersections, intersections_new, fn _, v1, v2 -> min(v1, v2) end)
+
+      if Enum.empty?(path) do
+        intersections
+      else
+        move_path(path, point, path_1_points, intersections, new_steps)
+      end
+    end
+
+    @doc """
+    Find a MapSet of points along the first wire's path
+    """
+    def find_wire_points(path) do
+      move_path(path, [0, 0], Map.new(), 0)
+    end
+
+    def find_intersections(paths) do
+      [path_1, path_2] = paths
+      path_1_points = find_wire_points(path_1)
+      move_path(path_2, [0, 0], path_1_points, Map.new(), 0)
+    end
   end
 
   def parse_input(input) do
-    paths = Utils.split_lines(input)
-    |> Enum.map(&String.split(&1, ",", trim: true))
+    paths =
+      Utils.split_lines(input)
+      |> Enum.map(&String.split(&1, ",", trim: true))
+
     paths
   end
 
-  def find_intersections(paths) do
-    [path_1, path_2] = paths
-    path_1_points = find_wire_points(path_1)
-    intersections = move_path(path_2, [0, 0], path_1_points, MapSet.new())
-    MapSet.to_list(intersections)
+  def part_one(paths) do
+    intersections = PartOne.find_intersections(paths)
+    PartOne.find_manhattan_distance_of_closest_intersection(intersections)
   end
 
-  def part_one(paths) do
-    intersections = find_intersections(paths)
-    find_manhattan_distance_of_closest_intersection(intersections)
+  def part_two(paths) do
+    PartTwo.find_intersections(paths)
+    |> Enum.min_by(fn {_, v} -> v end)
+    |> elem(1)
   end
 
   def main(filename \\ "./puzzle_inputs/day_3/paths.txt") do
     case File.read(filename) do
       {:ok, body} ->
         paths = parse_input(body)
-        answer = part_one(paths)
-        IO.puts(answer)
-      {:error, reason} -> IO.puts("Error: #{reason}")
-    end
 
+        answer = part_one(paths)
+        IO.puts("The Manhattan distance from the central port to the closest intersection is #{answer}")
+
+        answer = part_two(paths)
+        IO.puts("The fewest combined steps toan intersection is #{answer}")
+
+      {:error, reason} ->
+        IO.puts("Error: #{reason}")
+    end
   end
 end
--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -24,4 +24,28 @@ defmodule DayThreeTest do
     answer = DayThree.part_one(paths)
     assert answer == 135
   end
+
+  test "Part Two Example 1" do
+    paths = "R8,U5,L5,D3
+    U7,R6,D4,L4"
+    paths = DayThree.parse_input(paths)
+    answer = DayThree.part_two(paths)
+    assert answer == 30
+  end
+
+  test "Part Two Example 2" do
+    paths = "R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83"
+    paths = DayThree.parse_input(paths)
+    answer = DayThree.part_two(paths)
+    assert answer == 610
+  end
+
+  test "Part Two Example 3" do
+    paths = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+    paths = DayThree.parse_input(paths)
+    answer = DayThree.part_two(paths)
+    assert answer == 410
+  end
 end
